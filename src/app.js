ab55eff2d7cc3ae2de5f6209d158360229fc26a1
PabloGartzi <pablogartzi02@gmail.com>
2025-12-05 19:32:18 +0100

    Commit: cors actualizadas - URL DEL FRONT V2

--- src/app.js
+++ src/app.js
@@ -15,11 +15,13 @@ const port = process.env.PORT;
 var whitelist = ["https://server-yo1g.onrender.com", `http://localhost:${process.env.PORT}`, `http://localhost:3000`]
 var corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+    if (!origin) return callback(null, true);
+    if (whitelist.includes(origin)) {
+      return callback(null, true);
     }
+
+    console.log("Origin bloqueado:", origin);
+    callback(new Error("Not allowed by CORS"));
   }
 }
 app.use(cors(corsOptions))
